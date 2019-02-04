f0817c19afb2614684e895ad5eb189074f4c557a
JonathanBarrett92 <jobarret@tcd.ie>
2019-02-04 17:35:53 +0000

    Initial commit of library LibGDX generated files for project.

---
+++ desktop/src/com/treecrocs/flamabill/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.treecrocs.flamabill.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.treecrocs.flamabill.Flamabill;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Flamabill(), config);
+	}
+}
